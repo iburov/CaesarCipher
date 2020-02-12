17393748c0ece5538c0d9f329db3a0e92873a095
iburov.ca <iburov.ca@gmail.com>
2020-02-12 11:50:04 -0500

    save

--- main.php
+++ main.php
@@ -1,1 +1,19 @@
-hello world
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-5 col-lg-5">
+        <h3>Plaintext</h3>
+        <input class="plaintext">
+        <input class="shift" >
+    </div>
+    <div class="row col-xs-12 col-sm-12 col-md-2 col-lg-2 mt-2 mb-2">
+        <div class="test col-xs-6 col-sm-6 col-md-12 col-lg-12 mt-2 mb-2">
+            <button type="button" class="btn">Encrypt -></button>
+        </div>
+        <div class="test col-xs-6 col-sm-6 col-md-12 col-lg-12 mt-2 mb-2">
+            <button type="button" class="btn"><- Decrypt</button>
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-5 col-lg-5">
+        <h3>Cipher</h3>
+        <input class="cipher">
+    </div>
+</div>
--- nav.php
+++ nav.php
@@ -1,3 +1,12 @@
 <div class="nav">
-  <a class="" href=""><img src="Images/caesar.png" class="navlogo"></a>
+    <div class="col-2">
+        <a href="index.php"><img src="Images/caesar.png" class="navlogo"></a>
+    </div>
+
+    <div class="col-8">
+    </div>
+
+    <div class="col-2 test">
+        <a href="index.php" class="nav_title">Caesar Cipher</a>
+    </div>
 </div>
