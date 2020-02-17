00efab1bc33e581b1cc5f3db9a4488bf118d25cc
iburov.ca <iburov.ca@gmail.com>
2020-02-17 08:40:58 -0500

    added some inputs

--- main.php
+++ main.php
@@ -1,8 +1,25 @@
 <div class="row">
     <div class="col-xs-12 col-sm-12 col-md-5 col-lg-5">
         <h3>Plaintext</h3>
-        <input class="plaintext">
-        <input class="shift" >
+        <input type="text" class="plaintext text_input">
+        <input type="number" class="shift" placeholder="Shift">
+        <input type="checkbox" class="noshift" id="noshift">
+        <label for="noshift">No shift</label>
+
+        <div class="row encrypt_results">
+            <ol>
+                <li>Id ornare arcu odio ut</li>
+                <li>Id ornare arcu odio ut</li>
+                <li>Id ornare arcu odio ut</li>
+                <li>Id ornare arcu odio ut</li>
+                <li>Id ornare arcu odio ut</li>
+                <li>Id ornare arcu odio ut</li>
+                <li>Id ornare arcu odio ut</li>
+                <li>Id ornare arcu odio ut</li>
+                <li>Id ornare arcu odio ut</li>
+                <li>Id ornare arcu odio ut</li>
+            </ol>
+        </div>
     </div>
     <div class="row col-xs-12 col-sm-12 col-md-2 col-lg-2 mt-2 mb-2">
         <div class="test col-xs-6 col-sm-6 col-md-12 col-lg-12 mt-2 mb-2">
@@ -14,6 +31,21 @@
     </div>
     <div class="col-xs-12 col-sm-12 col-md-5 col-lg-5">
         <h3>Cipher</h3>
-        <input class="cipher">
+        <input type="text" class="cipher text_input">
+
+        <div class="row decrypt_results">
+            <ol>
+                <li>Id ornare arcu odio ut</li>
+                <li>Id ornare arcu odio ut</li>
+                <li>Id ornare arcu odio ut</li>
+                <li>Id ornare arcu odio ut</li>
+                <li>Id ornare arcu odio ut</li>
+                <li>Id ornare arcu odio ut</li>
+                <li>Id ornare arcu odio ut</li>
+                <li>Id ornare arcu odio ut</li>
+                <li>Id ornare arcu odio ut</li>
+                <li>Id ornare arcu odio ut</li>
+            </ol>
+        </div>
     </div>
 </div>
